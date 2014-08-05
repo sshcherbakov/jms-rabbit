57889208e04a2ff136a34d345b1fec8ab61774d7
sshcherbakov <sshcherbakov@gopivotal.com>
2014-08-05 10:51:28 +0200

    Add JndiTemplate configurable via property file for configuring remote JNDI source access

--- src/main/java/org/test/app/config/AppConfig.java
+++ src/main/java/org/test/app/config/AppConfig.java
@@ -1,5 +1,8 @@
 package org.test.app.config;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import javax.jms.ConnectionFactory;
 import javax.jms.Queue;
 
@@ -7,30 +10,48 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.jndi.JndiObjectFactoryBean;
+import org.springframework.jndi.JndiTemplate;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 public class AppConfig {
+	 
+	@Bean
+	public Properties jndiEnvironment() throws IOException {
+		Properties properties = new Properties();
+		properties.load(
+				Thread.currentThread().getContextClassLoader()
+				.getResourceAsStream("/jndi.properties"));
+		return properties;
+	}
 	
 	@Bean
-	public JndiObjectFactoryBean jmsConnectionFactory() {
+	public JndiTemplate jndiTemplate() throws IOException {
+		JndiTemplate jndiTemplate = new JndiTemplate(jndiEnvironment());
+		return jndiTemplate;
+	}
+	
+	@Bean
+	public JndiObjectFactoryBean jmsConnectionFactory(JndiTemplate jndiTemplate) {
 	
 		JndiObjectFactoryBean factoryBean = new JndiObjectFactoryBean();
 		factoryBean.setJndiName("java:comp/env/jms/myrabbit");
 		factoryBean.setExpectedType(ConnectionFactory.class);
 		factoryBean.setLookupOnStartup(false);
+		factoryBean.setJndiTemplate(jndiTemplate);
 		
 		return factoryBean;
 	}
 
 	@Bean
-	public JndiObjectFactoryBean jmsJndiQueue() {
+	public JndiObjectFactoryBean jmsJndiQueue(JndiTemplate jndiTemplate) {
 		
 		JndiObjectFactoryBean factoryBean = new JndiObjectFactoryBean();
 		factoryBean.setJndiName("java:comp/env/jms/test_queue");
 		factoryBean.setExpectedType(Queue.class);
 		factoryBean.setLookupOnStartup(false);
 		factoryBean.setProxyInterface(Queue.class);
+		factoryBean.setJndiTemplate(jndiTemplate);
 		
 		return factoryBean;
 		
